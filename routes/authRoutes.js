497995e59900d7e2abfd47c30af738a0bfa329e4
techiee3041 <doreenoms6699@gmail.com>
2024-04-15 11:27:24 +0300

    ✨ feat(authrouter): configure route
    configure route to fetch all users

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createUser, loginUser } = require("../controller/userCtr");
+const { createUser, loginUser, fetchUsers } = require("../controller/userCtr");
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
+router.get("/all-users", fetchUsers);
 
 module.exports = router;
