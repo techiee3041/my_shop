da959971cc105cace25c24257b9205772af74658
techiee3041 <doreenoms6699@gmail.com>
2024-04-15 16:46:02 +0300

    ✨ feat(userModel): schema modification
    added cart, address and wishlist to the schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,44 +1,50 @@
-const mongoose = require('mongoose'); 
+const mongoose = require("mongoose");
 const bycrypt = require("bcrypt");
 
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true,
-    },
-    lastName:{
-        type:String,
-        required:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-    role: {
-        type: String,
-        default: "user",
-    }
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  cart: {
+    type: Array,
+    default: [],
+  },
+  address: [{ type: ObjectId, ref: "Address" }],
+  wishList: [{ type: ObjectId, ref: "WishList" }],
 });
 
 userSchema.pre("save", async function (next) {
-    const salt = await bycrypt.genSaltSync(10);
-    this.password = await bycrypt.hash(this.password, salt);
+  const salt = await bycrypt.genSaltSync(10);
+  this.password = await bycrypt.hash(this.password, salt);
 });
 
 userSchema.methods.isPasswordMatch = async function (enteredPassword) {
-    return await bycrypt.compare(enteredPassword, this.password);
-}
+  return await bycrypt.compare(enteredPassword, this.password);
+};
 
 //Export the model
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
