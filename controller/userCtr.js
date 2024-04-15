7898354e77f8464616ba73d5685e7a74d8a2fedd
techiee3041 <doreenoms6699@gmail.com>
2024-04-15 11:09:13 +0300

    ✨ feat(userCtr.js): fetch users
    fetch all users

--- controller/userCtr.js
+++ controller/userCtr.js
@@ -2,6 +2,7 @@ const { generateToken } = require("../config/jwtToken");
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 
+//create user
 const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
   const findUser = await User.findOne({ email: email });
@@ -12,25 +13,35 @@ const createUser = asyncHandler(async (req, res) => {
     res.json(newUser);
   } else {
     // user already exists
-    throw new Error("User already exists")
+    throw new Error("User already exists");
   }
 });
 
-const loginUser = asyncHandler(async(req, res) => {
-  const {email, password} = req.body;
-  const findUser = await User.findOne({email});
-  if(findUser && findUser.isPasswordMatch(password)) {
+//login user
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const findUser = await User.findOne({ email });
+  if (findUser && findUser.isPasswordMatch(password)) {
     res.json({
       _id: findUser?._id,
       firstName: findUser?.firstName,
       lastName: findUser?.lastName,
       email: findUser?.email,
       mobile: findUser?.mobile,
-      token: generateToken(findUser?._id)
+      token: generateToken(findUser?._id),
     });
   } else {
     throw new Error("Invalid credentials");
   }
-})
+});
 
-module.exports = { createUser, loginUser };
+//fetch user
+const fetchUsers = asyncHandler(async (req, res) => {
+  try {
+    const getUsers = await User.find();
+    res.json(getUsers);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+module.exports = { createUser, loginUser, fetchUsers };
