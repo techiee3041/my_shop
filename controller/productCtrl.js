e5bfd4e97982565da844682472d5b79679ad519d
techiee3041 <doreenoms6699@gmail.com>
2024-05-14 17:37:21 +0300

    ✨ feat(productCtrl): handle pagination

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -48,33 +48,73 @@ const updateProduct = asyncHandler(async (req, res) => {
 });
 
 //delete a product
-const deleteProduct = asyncHandler(async(req, res) => {
+const deleteProduct = asyncHandler(async (req, res) => {
   const { id } = req.params;
   try {
     const deletedProduct = await Product.findOneAndDelete({ _id: id });
-    if(!deletedProduct) {
+    if (!deletedProduct) {
       return res.status(404).json({ message: "Product not found" });
     }
-    res.json({ message: "Product deleted", deletedProduct})
+    res.json({ message: "Product deleted", deletedProduct });
   } catch (error) {
     throw new Error(error);
   }
-})
+});
 
 //fetching all products
 const fetchProducts = asyncHandler(async (req, res) => {
   try {
     //filtering products
-    const queryObject = {...req.query};
+    const queryObject = { ...req.query };
     const excludeFields = ["page", "sort", "limit", "fields"];
     excludeFields.forEach((el) => delete queryObject[el]);
     let queryString = JSON.stringify(queryObject);
-    queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    const products = await Product.find(JSON.parse(queryString));
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+
+    let queriedProduct = Product.find(JSON.parse(queryString));
+
+    //sort products
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      queriedProduct = queriedProduct.sort(sortBy);
+    } else {
+      queriedProduct = queriedProduct.sort("-createdAt");
+    }
+
+    //limiting the fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      queriedProduct = queriedProduct.select(fields);
+    } else {
+      queriedProduct = queriedProduct.select("-__v")
+    }
+
+    //pagination
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    queriedProduct = queriedProduct.skip(skip).limit(limit);
+    if (page) {
+      const productCount = await Product.countDocuments();
+      if (skip >= productCount) {
+        throw new Error("This page does not exist")
+      }
+    }
+
+    const products = await queriedProduct;
     res.json(products);
   } catch (error) {
     throw new Error(error);
   }
 });
 
-module.exports = { createProduct, getProduct, fetchProducts, updateProduct, deleteProduct };
+module.exports = {
+  createProduct,
+  getProduct,
+  fetchProducts,
+  updateProduct,
+  deleteProduct,
+};
